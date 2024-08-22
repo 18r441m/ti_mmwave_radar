e6ab5f546987b767a0fffebf145d40bc95e2a668
18r441m <18r441m@gmail.com>
2024-08-22 16:52:50 -0400

    some changes based on docs

--- lua/cascade_steam_and_capture.lua
+++ lua/cascade_steam_and_capture.lua
@@ -1,56 +1,55 @@
 local host_ip = "192.168.33.50"
-local data_port = 4098
-local command_port = 5000
+local fpga_ip = "192.168.33.30"  -- Placeholder for FPGA destination IP address
+local fpga_mac = "AA:BB:CC:DD:EE:FF"  -- Placeholder for FPGA MAC address
+local config_port = 4096  -- Placeholder for the config port
+local record_port = 4098  -- Port used for streaming data
+
+local radar_devices = {4, 3, 2, 1}  -- Device IDs for slaves (3, 2, 1) and master (1)
 
 function ConfigureEthernet()
-    ar1.CaptureCardConfig_EthInit_mult(1, host_ip, data_port)
+    -- Initialize Ethernet with the provided host and FPGA information
+    ar1.CaptureCardConfig_EthInit(host_ip, fpga_ip, fpga_mac, config_port, record_port)
     WriteToLog("Ethernet configuration for specific host IP successful.\n", "green")
+end
+
+function ConfigureRadarDevices()
+    -- Loop through each radar device in the cascade and configure it for continuous streaming
+    for _, RadarDeviceId in ipairs(radar_devices) do
+        local startFreqConst = 77.0 -- Example start frequency, modify as needed
+        local digOutSampleRate = 8000 -- Example digital output sample rate, modify as needed
+        local rxGain = 30 -- Example RX gain, modify as needed
+        local hpfCornerFreq1 = 0 -- Example HPF corner frequency 1, modify as needed
+        local hpfCornerFreq2 = 0 -- Example HPF corner frequency 2, modify as needed
+        local tx0OutPowerBackoffCode = 0 -- Example TX power backoff code for TX0, modify as needed
+        local tx1OutPowerBackoffCode = 0 -- Example TX power backoff code for TX1, modify as needed
+        local tx2OutPowerBackoffCode = 0 -- Example TX power backoff code for TX2, modify as needed
+        local tx0PhaseShifter = 0 -- Example phase shifter value for TX0, modify as needed
+        local tx1PhaseShifter = 0 -- Example phase shifter value for TX1, modify as needed
+        local tx2PhaseShifter = 0 -- Example phase shifter value for TX2, modify as needed
+
+        -- Configure continuous streaming for this radar device
+        ar1.ContStrConfig_mult(RadarDeviceId, startFreqConst, digOutSampleRate, rxGain, hpfCornerFreq1, hpfCornerFreq2, 
+                               tx0OutPowerBackoffCode, tx1OutPowerBackoffCode, tx2OutPowerBackoffCode, 
+                               tx0PhaseShifter, tx1PhaseShifter, tx2PhaseShifter)
+        
+        WriteToLog("Configured continuous streaming for Radar Device " .. RadarDeviceId .. ".\n", "blue")
+    end
 end
 
 function StartContinuousStreaming()
     ConfigureEthernet()
+    ConfigureRadarDevices()
 
-    ar1.CaptureCardConfig_Mode_mult(1, 1)
-
-    WriteToLog("Started continuous streaming over Ethernet to specific host.\n", "blue")
-
-    if (RadarDevice[4]==1) then Framing_Control(4, 1) end
-    if (RadarDevice[3]==1) then Framing_Control(3, 1) end
-    if (RadarDevice[2]==1) then Framing_Control(2, 1) end
-    Framing_Control(1, 1)
-end
-
-function StopContinuousStreaming()
-    if (RadarDevice[4]==1) then Framing_Control(4, 0) end
-    if (RadarDevice[3]==1) then Framing_Control(3, 0) end
-    if (RadarDevice[2]==1) then Framing_Control(2, 0) end
-    Framing_Control(1, 0)
-
-    ar1.CaptureCardConfig_StopRecord_mult(1)
-    WriteToLog("Stopped continuous streaming to specific host.\n", "blue")
-end
-
-function ListenForCommands()
-    local udpSocket = assert(socket.udp())
-    udpSocket:setsockname("*", command_port)
-    udpSocket:settimeout(0)
-
-    while true do
-        local command, ip, port = udpSocket:receivefrom()
-        if command then
-            WriteToLog("Received command: " .. command .. " from " .. ip .. "\n", "yellow")
-            if command == "START" then
-                StartContinuousStreaming()
-            elseif command == "STOP" then
-                StopContinuousStreaming()
-            else
-                WriteToLog("Unknown command: " .. command .. "\n", "red")
-            end
-        end
-        RSTD.Sleep(100)
+    -- Enable continuous streaming mode in the order: Slaves (4, 3, 2), then Master (1)
+    for i = 1, #radar_devices - 1 do  -- Trigger all slaves first
+        ar1.ContStrModEnable_mult(radar_devices[i])
+        WriteToLog("Started continuous streaming for Slave Device " .. radar_devices[i] .. ".\n", "blue")
     end
 
-    udpSocket:close()
+    -- Finally, trigger the Master device
+    ar1.ContStrModEnable_mult(radar_devices[#radar_devices])
+    WriteToLog("Started continuous streaming for Master Device " .. radar_devices[#radar_devices] .. ".\n", "blue")
 end
 
-ListenForCommands()
+-- Start the streaming process automatically
+StartContinuousStreaming()
